b582169e91a7820a01313b7dd8f5622e1252635c
PowerOleg <geraskin.oleg@mail.ru>
2022-11-15 02:39:01 +0500

    Comparator's constructor is configured

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,15 @@
-import java.util.*;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class Main {
     public static void main(String[] args) {
         Person person1 = new Person("Roland", "Salimr Ibn Sali Barad", 60);
         Person person2 = new Person("Roman", "Genry Robertson", 100);
-        PersonComparator personComparator = new PersonComparator();
+        PersonComparator personComparator = new PersonComparator(2);
         PersonComparator2 personComparator2 = new PersonComparator2();
         Set<Person> set = new TreeSet<Person>(personComparator.thenComparing(personComparator2));
-
         set.add(new Person("Alena", "Veselovskih", 25));
         set.add(person2);
         set.add(new Person("Oleg", "Veselovskih ", 30));
@@ -18,14 +20,24 @@ public class Main {
         }
     }
 }
+
 class PersonComparator implements Comparator<Person> {
+    int maxSurnameWords;
+    public PersonComparator(int maxSurnameWords) {
+        this.maxSurnameWords = maxSurnameWords;
+    }
     @Override
     public int compare(Person o1, Person o2) {
-       String[] surnames1 = o1.getSurname().split(" ");
-       String[] surnames2 = o2.getSurname().split(" ");
-       return Integer.compare(surnames1.length, surnames2.length) * -1;
+        String[] surnames1 = o1.getSurname().split(" ");
+        String[] surnames2 = o2.getSurname().split(" ");
+        int s1 = surnames1.length;
+        int s2 = surnames2.length;
+        if (surnames1.length > maxSurnameWords) s1 = 2;
+        if (surnames2.length > maxSurnameWords) s2 = 2;
+        return Integer.compare(s1, s2) * -1;
     }
 }
+
 class PersonComparator2 implements Comparator<Person> {
     @Override
     public int compare(Person o1, Person o2) {
