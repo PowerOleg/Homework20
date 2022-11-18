5d783e7b4a2e9304dc5a6365396e10685f4318a5
PowerOleg <geraskin.oleg@mail.ru>
2022-11-19 01:14:20 +0500

    Now there is one comparator

--- src/Main.java
+++ src/Main.java
@@ -7,12 +7,12 @@ public class Main {
         Person<String> person1 = new Person("Roland", "Salimr Ibn Sali Barad", 60);
         Person<String> person2 = new Person("Roman", "Genry Robertson", 100);
         PersonComparator personComparator = new PersonComparator(2);
-        PersonComparator2 personComparator2 = new PersonComparator2();
-        Set<Person<String>> set = new TreeSet<>(personComparator.thenComparing(personComparator2));
-        set.add(new Person<String>("Alena", "Veselovskih", 25));
+        Set<Person<String>> set = new TreeSet<>(personComparator);
+        set.add(new Person<>("Alena", "Veselovskih", 25));
         set.add(person2);
-        set.add(new Person<String>("Oleg", "Veselovskih ", 30));
+        set.add(new Person<>("Oleg", "Veselovskih ", 30));
         set.add(person1);
+        set.add(new Person<>("Muhammad", "Lagni Malani Rhan Ali Mali Karah", 12));
         for (Iterator<Person<String>> iterator = set.iterator(); iterator.hasNext(); ) {
             Person p = iterator.next();
             System.out.println(p);
@@ -31,14 +31,11 @@ class PersonComparator implements Comparator<Person<String>> {
         String[] surnames2 = o2.getSurname().split(" ");
         int s1 = surnames1.length;
         int s2 = surnames2.length;
-        if (surnames1.length > maxSurnameWords) s1 = 2;
-        if (surnames2.length > maxSurnameWords) s2 = 2;
-        return Integer.compare(s1, s2) * -1;
+        if (surnames1.length > maxSurnameWords) s1 = maxSurnameWords;
+        if (surnames2.length > maxSurnameWords) s2 = maxSurnameWords;
+        if (s1 != s2) {
+            return Integer.compare(s1, s2) * -1;
+        } else return Integer.compare(o1.getAge(), o2.getAge()) * -1;
     }
 }
-class PersonComparator2 implements Comparator<Person> {
-    @Override
-    public int compare(Person o1, Person o2) {
-        return Integer.compare(o1.getAge(), o2.getAge()) * -1;
-    }
-}
+
