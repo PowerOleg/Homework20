03bd7d11cce5d24fa40ae329d8944ac6149f232f
PowerOleg <geraskin.oleg@mail.ru>
2022-11-19 15:42:35 +0500

    ClassCastExeption

--- src/Main.java
+++ src/Main.java
@@ -2,31 +2,40 @@ import java.util.Comparator;
 import java.util.Iterator;
 import java.util.Set;
 import java.util.TreeSet;
+
 public class Main {
-    public static void main(String[] args) {
-        Person<String> person1 = new Person("Roland", "Salimr Ibn Sali Barad", 60);
-        Person<String> person2 = new Person("Roman", "Genry Robertson", 100);
-        PersonComparator personComparator = new PersonComparator(2);
-        Set<Person<String>> set = new TreeSet<>(personComparator);
-        set.add(new Person<>("Alena", "Veselovskih", 25));
-        set.add(person2);
-        set.add(new Person<>("Oleg", "Veselovskih ", 30));
-        set.add(person1);
-        set.add(new Person<>("Muhammad", "Lagni Malani Rhan Ali Mali Karah", 12));
-        for (Iterator<Person<String>> iterator = set.iterator(); iterator.hasNext(); ) {
+
+    public static Set<Person> fillCollection(Comparator<Person> personComparator) {
+        Set<Person> set = new TreeSet<>(personComparator);
+        set.add(new Person("Alena", "Veselovskih", 25));
+        set.add(new Person("Roman", "Genry Robertson", 100));
+        set.add(new Person("Oleg", "Veselovskih ", 30));
+        set.add(new Person("Roland", "Salimr Ibn Sali Barad", 60));
+        set.add(new Person("Muhammad", "Lagni Malani Rhan Ali Mali Karah", 12));
+        return set;
+    }
+
+    public static void print(Set<Person> set) {
+        for (Iterator<Person> iterator = set.iterator(); iterator.hasNext(); ) {
             Person p = iterator.next();
             System.out.println(p);
         }
     }
+
+    public static void main(String[] args) {
+        PersonComparator personComparator = new PersonComparator(2);
+        Set<Person> set = new TreeSet<>(fillCollection(personComparator));
+        print(set);
+    }
 }
 
-class PersonComparator implements Comparator<Person<String>> {
+class PersonComparator implements Comparator<Person> {
     int maxSurnameWords;
     public PersonComparator(int maxSurnameWords) {
         this.maxSurnameWords = maxSurnameWords;
     }
     @Override
-    public int compare(Person<String> o1, Person<String> o2) {
+    public int compare(Person o1, Person o2) {
         String[] surnames1 = o1.getSurname().split(" ");
         String[] surnames2 = o2.getSurname().split(" ");
         int s1 = surnames1.length;
@@ -35,7 +44,8 @@ class PersonComparator implements Comparator<Person<String>> {
         if (surnames2.length > maxSurnameWords) s2 = maxSurnameWords;
         if (s1 != s2) {
             return Integer.compare(s1, s2) * -1;
-        } else return Integer.compare(o1.getAge(), o2.getAge()) * -1;
+        }
+        return Integer.compare(o1.getAge(), o2.getAge()) * -1;
     }
 }
 
--- src/Person.java
+++ src/Person.java
@@ -1,17 +1,17 @@
-public class Person<T> {
-    private T name;
-    private T surname;
+public class Person {
+    private String name;
+    private String surname;
     private int age;
 
-    public Person(T name, T surname, int age) {
+    public Person(String name, String surname, int age) {
         this.name = name;
         this.surname = surname;
         this.age = age;
     }
-    public T getName() {return name;}
-    public void setName(T name) {this.name = name;}
-    public T getSurname() {return surname;}
-    public void setSurname(T surname) {this.surname = surname;}
+    public String getName() {return name;}
+    public void setName(String name) {this.name = name;}
+    public String getSurname() {return surname;}
+    public void setSurname(String surname) {this.surname = surname;}
     public int getAge() {return age;}
     public void setAge(int age) {this.age = age;}
 
