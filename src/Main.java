de31d85e922f93e4e675d13654f8fc68ba9343a0
PowerOleg <geraskin.oleg@mail.ru>
2022-11-29 00:35:56 +0500

    LambdaComparator

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,26 @@ import java.util.Comparator;
 import java.util.Iterator;
 import java.util.Set;
 import java.util.TreeSet;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 public class Main {
 
     public static Set<Person> fillCollection() {
-        Set<Person> set = new TreeSet<>();
+        Set<Person> set = new TreeSet<>( (o1, o2) -> {
+                int maxSurnameWords = 2;
+                String[] surnames1 = o1.getSurname().split(" ");
+                String[] surnames2 = o2.getSurname().split(" ");
+                int s1 = surnames1.length;
+                int s2 = surnames2.length;
+                if (surnames1.length > maxSurnameWords) s1 = maxSurnameWords;
+                if (surnames2.length > maxSurnameWords) s2 = maxSurnameWords;
+                if (s1 != s2) {
+                    return Integer.compare(s1, s2) * -1;
+                }
+                return Integer.compare(o1.getAge(), o2.getAge()) * -1;
+        });
+
         set.add(new Person("Alena", "Veselovskih", 25));
         set.add(new Person("Roman", "Genry Robertson", 100));
         set.add(new Person("Oleg", "Veselovskih ", 110));
@@ -22,30 +37,13 @@ public class Main {
         }
     }
     public static void main(String[] args) {
-        PersonComparator personComparator = new PersonComparator(2);
-        Set<Person> set = new TreeSet<>(personComparator);
-        set.addAll(fillCollection());
-        print(set);
+        Supplier<Set<Person>> supplier = Main::fillCollection;
+        print(supplier.get());
+        System.out.println("Another approach, the same result");
+        fillCollection().stream().forEach(System.out::println);
     }
 }
 
-class PersonComparator implements Comparator<Person> {
-    int maxSurnameWords;
-    public PersonComparator(int maxSurnameWords) {
-        this.maxSurnameWords = maxSurnameWords;
-    }
-    @Override
-    public int compare(Person o1, Person o2) {
-        String[] surnames1 = o1.getSurname().split(" ");
-        String[] surnames2 = o2.getSurname().split(" ");
-        int s1 = surnames1.length;
-        int s2 = surnames2.length;
-        if (surnames1.length > maxSurnameWords) s1 = maxSurnameWords;
-        if (surnames2.length > maxSurnameWords) s2 = maxSurnameWords;
-        if (s1 != s2) {
-            return Integer.compare(s1, s2) * -1;
-        }
-        return Integer.compare(o1.getAge(), o2.getAge()) * -1;
-    }
-}
+
+
 
