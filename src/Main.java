292357623bdb88a10cabece246a5387e3fd2f783
PowerOleg <geraskin.oleg@mail.ru>
2022-11-29 23:35:26 +0500

    No ComparatorLogic

--- src/Main.java
+++ src/Main.java
@@ -2,29 +2,9 @@ import java.util.*;
 
 public class Main {
 
-    public static Set<Person> fillCollection(int maxSurnameWords) {
-        Set<Person> set = new TreeSet<>(new Comparator<Person>() {
-            @Override
-            public int compare(Person o1, Person o2) {
-                String[] surnames1 = o1.getSurname().split(" ");
-                String[] surnames2 = o2.getSurname().split(" ");
-                int s1 = surnames1.length;
-                int s2 = surnames2.length;
-                if (surnames1.length > maxSurnameWords) s1 = maxSurnameWords;
-                if (surnames2.length > maxSurnameWords) s2 = maxSurnameWords;
-                if (s1 != s2) {
-                    return Integer.compare(s1, s2) * -1;
-                }
-                return Integer.compare(o1.getAge(), o2.getAge()) * -1;
-            }
-        });
-        set.add(new Person("Alena", "Veselovskih", 25));
-        set.add(new Person("Roman", "Genry Robertson", 100));
-        set.add(new Person("Oleg", "Veselovskih ", 110));
-        set.add(new Person("Roland", "Salimr Ibn Sali Barad", 60));
-        set.add(new Person("Muhammad", "Lagni Malani Rhan Ali Mali Karah", 12));
-        return set;
-    }
+
+
+
 
     public static void print(Set<Person> set) {
         for (Iterator<Person> iterator = set.iterator(); iterator.hasNext(); ) {
@@ -34,9 +14,16 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        Set<Person> set = fillCollection(2);
+        Set<Person> set = new TreeSet<>(Comparator.comparing(Person::getAge));
+        set.add(new Person("Alena", "Veselovskih", 25));
+        set.add(new Person("Roman", "Genry Robertson", 100));
+        set.add(new Person("Oleg", "Veselovskih ", 110));
+        set.add(new Person("Roland", "Salimr Ibn Sali Barad", 60));
+        set.add(new Person("Muhammad", "Lagni Malani Rhan Ali Mali Karah", 12));
         print(set);
+
     }
+
 }
 
 
